c20d5183c9875295ebbcb97b01a48e4ef38a1ac8
DawidPajak1 <166152921+DawidPajak1@users.noreply.github.com>
2024-04-06 14:25:38 +0200

    while_dawid_pajak.cpp

--- while_dawid_pajak.cpp
+++ while_dawid_pajak.cpp
@@ -7,7 +7,7 @@ int main()
     while(miejscowoscZamieszkania<=100000000)
     {
         iloscMinutDojazdu +=7;
-        miejscowoscZamieszkania +=2;
+        miejscowoscZamieszkania *=2;
         cout << "Mineło minut: " << iloscMinutDojazdu << endl;
         cout << "Liczba zamieszkanych osób: " << miejscowoscZamieszkania << endl;
     }
